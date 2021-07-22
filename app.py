fc91b7ccc93c1c8d951bc518215f11862679fc55
Abhishek Soni <50213022+abhisheksoni-iitb@users.noreply.github.com>
2021-07-23 01:19:56 +0530

    Corrections

--- app.py
+++ app.py
@@ -15,55 +15,54 @@ app = Flask(__name__)
 # filename = 'global.model'
 # model = joblib.load(open(filename, 'rb'))
 
-model = xgb.Booster()
-model.load_model('test_model.bin')
+# model = xgb.Booster()
+# model.load_model('test_model.bin')
+
+model = xgb.XGBClassifier()
+model.load_model("model_sklearn.json")
 
 def analomalies(features):
     # if features[0] 
 
-    if features[0] < 2.18 or features[0] > 4.03:
+    
+    if features[0] < 1.5 or features[0] >2.63:
         return 1
     
-    if features[1] < 1.5 or features[1] >2.63:
+    if features[1] < 32.4 or features[1] > 47.4:
         return 1
     
-    if features[2] < 32.4 or features[2] > 47.4:
+    if features[2] > 1600 or features[2] <1200:
         return 1
     
-    if features[3] > 1600 or features[3] <1200:
+    if features[3] > 47 or features[3] < 42:
         return 1
     
-    if features[4] > 47 or features[4] < 42:
+    if features[4] > 59.5 or features[4] <0:
         return 1
     
-    if features[5] > 59.5 or features[5] <0:
+    if features[5] > 22.94 or features[5] <0:
         return 1
     
-    if features[6] > 22.94 or features[6] <0:
-        return 1
     
-    # if features[1] > 0 or features[1] <2.63:
-    #     return 1
-    
-    if features[8] > 1650 or features[8] <1500:
+    if features[6] > 1650 or features[6] <1500:
         return 1
 
-    if features[9] > 135 or features[9] <100:
+    if features[7] > 135 or features[7] <100:
         return 1
 
-    if features[10] > 10.96 or features[10] <9.42:
+    if features[8] > 10.96 or features[8] <9.42:
         return 1
 
-    if features[11] > 8.37 or features[11] <7.35:
+    if features[9] > 8.37 or features[9] <7.35:
         return 1
 
-    if features[12] > 2.78 or features[12] <1.91:
+    if features[10] > 2.78 or features[10] <1.91:
         return 1
 
-    if features[13] > 3.8 or features[13] <2.8:
+    if features[11] > 3.8 or features[11] <2.8:
         return 1
 
-    if features[14] > 5.5 or features[14] <4.65:
+    if features[12] > 5.5 or features[12] <4.65:
         return 1
 
     return 0
@@ -83,21 +82,19 @@ def predict():
     print(len(features))
     
     dictionary = {
-                    'Set_H2O_act':[features[0]],
-                    'Meas_H2O':[features[1]],
-                    'Meas_Temp':[features[2]],
-                    'Weight':[features[3]],
-                    'Set_Comp':[features[4]],
-                    'Meas_Comp1':[features[5]],
-                    'Lit_fin1':[features[6]],
-                    'Lit_fin2':[features[7]],
-                    'GCS':[features[8]],
-                    'Permeability':[features[9]],
-                    'total_Clay':[features[10]],
-                    'active_clay':[features[11]],
-                    'Dead_clay_':[features[12]],
-                    'V_M_':[features[13]],
-                    'LOI':[features[14]]
+                    'Meas_H2O':[features[0]],
+                    'Meas_Temp':[features[1]],
+                    'Weight':[features[2]],
+                    'Set_Comp':[features[3]],
+                    'Meas_Comp1':[features[4]],
+                    'Lit_fin1':[features[5]],
+                    'GCS':[features[6]],
+                    'Permeability':[features[7]],
+                    'total_Clay':[features[8]],
+                    'active_clay':[features[9]],
+                    'Dead_clay_':[features[10]],
+                    'V_M_':[features[11]],
+                    'LOI':[features[12]]
                 }
     # print(features)
 
@@ -106,17 +103,18 @@ def predict():
     if condition != 1:
         final_features = pd.DataFrame(data=dictionary)
         
-        final_features = xgb.DMatrix(final_features.values)
+        # final_features = xgb.DMatrix(final_features.values)
 
         prediction = model.predict(final_features)
 
-        output = round(prediction[0], 2)
+        output = prediction
     # output = len(features)
 
     else:
         output = 1
 
-    if output < 0.5:
+
+    if output == 0:
         output = 'Non-defective mould'
     else:
         output = 'Defective mould'
