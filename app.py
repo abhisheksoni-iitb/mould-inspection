6c476b7aed53c453142114696d3f73cab73d29b6
Abhishek Soni <50213022+abhisheksoni-iitb@users.noreply.github.com>
2021-07-23 00:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,56 @@ app = Flask(__name__)
 model = xgb.Booster()
 model.load_model('test_model.bin')
 
+def analomalies(features):
+    # if features[0] 
 
+    if features[0] < 2.18 or features[0] > 4.03:
+        return 1
+    
+    if features[1] < 1.5 or features[1] >2.63:
+        return 1
+    
+    if features[2] < 32.4 or features[2] > 47.4:
+        return 1
+    
+    if features[3] > 1600 or features[3] <1200:
+        return 1
+    
+    if features[4] > 47 or features[4] < 42:
+        return 1
+    
+    if features[5] > 59.5 or features[5] <0:
+        return 1
+    
+    if features[6] > 22.94 or features[6] <0:
+        return 1
+    
+    # if features[1] > 0 or features[1] <2.63:
+    #     return 1
+    
+    if features[8] > 1650 or features[8] <1500:
+        return 1
+
+    if features[9] > 135 or features[9] <100:
+        return 1
+
+    if features[10] > 10.96 or features[10] <9.42:
+        return 1
+
+    if features[11] > 8.37 or features[11] <7.35:
+        return 1
+
+    if features[12] > 2.78 or features[12] <1.91:
+        return 1
+
+    if features[13] > 3.8 or features[13] <2.8:
+        return 1
+
+    if features[14] > 5.5 or features[14] <4.65:
+        return 1
+
+    return 0
+    
 
 @app.route('/')
 def home():
@@ -50,16 +99,22 @@ def predict():
                     'V_M_':[features[13]],
                     'LOI':[features[14]]
                 }
-    print(features)
+    # print(features)
 
-    final_features = pd.DataFrame(data=dictionary)
-    
-    final_features = xgb.DMatrix(final_features.values)
+    condition = analomalies(features)
 
-    prediction = model.predict(final_features)
+    if condition != 1:
+        final_features = pd.DataFrame(data=dictionary)
+        
+        final_features = xgb.DMatrix(final_features.values)
 
-    output = round(prediction[0], 2)
+        prediction = model.predict(final_features)
+
+        output = round(prediction[0], 2)
     # output = len(features)
+
+    else:
+        output = 1
 
     if output < 0.5:
         output = 'Non-defective mould'
